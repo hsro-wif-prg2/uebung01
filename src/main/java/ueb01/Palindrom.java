566926fc39d4d62681dd5b9f88a2da6eba28b9f2
feridtola <ferid-tola@hotmail.de>
2018-03-28 18:01:23 +0200

    LoesungUebung01--

--- src/main/java/ueb01/Palindrom.java
+++ src/main/java/ueb01/Palindrom.java
@@ -4,6 +4,29 @@ class Palindrom {
 	static boolean istPalindrom(String s) {
 		// Verwenden Sie die Klasse CharStackImpl um auf Palindrom zu testen!
 
-		return false;
+		s = s.replaceAll("\\s+","");
+		s = s.toLowerCase();
+
+		if(s == null)
+			throw new NullPointerException();
+
+		int length = s.length();
+
+		// Wenn String nur eine Buchstabe dann true
+		if (length < 2) // If the string only has 1 char or is empty
+			return true;
+
+		else {
+
+		// Wenn die erste Buchstabe mit der letzten nicht übereinstimmt dann false
+			if (s.charAt(0) != s.charAt(length - 1))
+				return false;
+
+		//		// Function call for string with the two ends snipped off
+			else
+				return istPalindrom(s.substring(1, length - 1));
+		}
+
+
 	}
 }
---
+++ src/main/java/ueb01/StackClass.java
@@ -0,0 +1,13 @@
+package ueb01;
+
+public class StackClass {
+
+    char value;
+    StackClass next;
+
+    public StackClass(char value, StackClass next){
+        this.value = value;
+        this.next = next;
+
+    }
+}
