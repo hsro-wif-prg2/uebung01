844c5c1ef144bf46d4ffe4f065fdf5420d6fc63f
Anton <d1Av0lo3124@gmail.com>
2018-04-02 20:41:02 +0200

    loesung1

--- src/main/java/ueb01/Palindrom.java
+++ src/main/java/ueb01/Palindrom.java
@@ -2,8 +2,15 @@ package ueb01;
 
 class Palindrom {
 	static boolean istPalindrom(String s) {
-		// Verwenden Sie die Klasse CharStackImpl um auf Palindrom zu testen!
 
-		return false;
+			// Verwenden Sie die Klasse CharStackImpl um auf Palindrom zu testen!
+             CharStack cs = new CharStackImpl();
+             s = s.replaceAll("\\s","");
+		     s = s.toUpperCase();
+			 char [] charastr = s.toCharArray();
+			return false;
+		}
 	}
-}
+
+
+
