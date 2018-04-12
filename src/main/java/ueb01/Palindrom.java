b41c0206b815a418de4e55db4f6b58b1267ca9a8
E-PC\E <eugen.zeeb@yahoo.de>
2018-04-12 21:08:05 +0200

    A

--- src/main/java/ueb01/Palindrom.java
+++ src/main/java/ueb01/Palindrom.java
@@ -3,7 +3,19 @@ package ueb01;
 class Palindrom {
 	static boolean istPalindrom(String s) {
 		// Verwenden Sie die Klasse CharStackImpl um auf Palindrom zu testen!
-
+			String stringNew1 =s.replaceAll(" ", "");
+			String stringNew = stringNew1.toLowerCase();
+			CharStackImpl stack1 = new CharStackImpl();
+			for(int i=0; i<stringNew.length(); i++) {
+				stack1.push(stringNew.charAt(i));
+			}
+			String string1 = "";
+			for(int i=0; i<stringNew.length(); i++) {
+				string1 = string1 + stack1.pop();
+			}
+			if(string1.equals(stringNew)) {
+				return true;
+			}
 		return false;
 	}
 }
