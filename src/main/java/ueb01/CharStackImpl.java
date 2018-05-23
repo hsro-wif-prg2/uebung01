ef2a1a668567866e213a89a7c6dbca74ef586b8e
Gornung <37617440+Gornung@users.noreply.github.com>
2018-05-23 13:44:00 +0200

    Teillösung 1

--- src/main/java/ueb01/CharStackImpl.java
+++ src/main/java/ueb01/CharStackImpl.java
@@ -1,4 +1,32 @@
 package ueb01;
 
-class CharStackImpl implements CharStack {
+import java.util.NoSuchElementException;
+
+public class CharStackImpl implements CharStack {
+    private CharElement top;
+    @Override
+    public void push(char c) {
+        top = new CharElement(c, top);
+    }
+
+    @Override
+    public char pop() {
+        if(top == null) {
+            throw new NoSuchElementException("keine Elemente!");
+        }
+        char v = top.value;
+        top = top.next;
+        return v;
+    }
+
+    @Override
+    public int size() {
+        int n = 0;
+        CharElement it = top;
+        while (it != null) {
+                n++;
+                it = it.next;
+        }
+        return n;
+    }
 }
